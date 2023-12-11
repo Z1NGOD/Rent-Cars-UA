5eda9ca1c38cddc573e128abf998c848126e3dfa
Z1NG0D <YaroslavBorovskiy@gmail.com>
2023-12-12 00:22:26 +0100

    react strickt mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistor, store } from "./Redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="">
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
