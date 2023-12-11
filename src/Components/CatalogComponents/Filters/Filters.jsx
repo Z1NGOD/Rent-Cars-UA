9e4f823069a0bb690bf69391acc22836af839c96
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2023-12-11 02:50:33 +0200

    started to make filters

--- src/Components/CatalogComponents/Filters/Filters.jsx
+++ src/Components/CatalogComponents/Filters/Filters.jsx
@@ -1,66 +1,120 @@
+// import { Form, Label, LeftInput, RightInput, SelectContainer, Select, Option } from "../Catalog.styled";
+// import { Button } from "../../UI/Button.styed";
 import {
-  Form,
-  Label,
-  LeftInput,
-  RightInput,
-  SelectContainer,
-  Select,
-  Option,
+  FormContainer,
+  SelectCarBrandContainer,
+  SelectBtnTitle,
+  SelectBtn,
+  SelectBtnText,
+  SelectBtnIcon,
+  SelectContent,
+  SelectOptions,
+  SelectOptionsItem,
 } from "./Filters.styled";
-import { Button } from "../../UI/Button.styed";
+
+import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { getFiltersFetch } from "../../../Redux/filtersSlice";
 import { nanoid } from "nanoid";
-import Loader from "../../UI/Loader";
+import icons from "../../../images/icons.svg";
 
 const CatalogFilters = () => {
+  const [brandValue, setBrandValue] = useState(null);
+  const [isBrandDropDownOpen, setIsBrandDropDownOpen] = useState(false);
+
   const dispatch = useDispatch();
   const filters = useSelector((state) => state.filters.filters);
   const isLoading = useSelector((state) => state.filters.loading);
+
   useEffect(() => {
     dispatch(getFiltersFetch());
+
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target) && !selectButton.contains(event.target)) {
+        setIsBrandDropDownOpen(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
   }, [dispatch]);
 
+  const dropdownRef = useRef(null);
   return (
     <>
       {isLoading ? (
-        <Loader />
+        <></>
       ) : (
-        <Form>
-          <Label>
-            Car brand
-            <SelectContainer>
-              <Select>
-                {filters.map((filter) => (
-                  <Option key={nanoid()}>{filter}</Option>
-                ))}
-              </Select>
-            </SelectContainer>
-          </Label>
-          <Label>
-            Price/1h
-            <Select>
-              <Option>To 10$</Option>
-              <Option>To 20$</Option>
-              <Option>To 30$</Option>
-              <Option>To 40$</Option>
-              <Option>To 50$</Option>
-            </Select>
-          </Label>
-          <div style={{ display: "flex", alignItems: "flex-end" }}>
-            <Label>
-              Сar mileage / km
-              <LeftInput type="text" borderSide={true} placeholder="From" />
-            </Label>
-            <RightInput borderSide={false} placeholder="To" />
-          </div>
+        <FormContainer>
+          <SelectCarBrandContainer>
+            <SelectBtnTitle>Car brand</SelectBtnTitle>
+            <SelectBtn
+              id="selectButton"
+              onClick={() => {
+                setIsBrandDropDownOpen(!isBrandDropDownOpen);
+              }}
+            >
+              <SelectBtnText>{brandValue ? brandValue : "Enter the text"}</SelectBtnText>
+              <SelectBtnIcon width={20} height={20} isBrandDropDownOpen={isBrandDropDownOpen}>
+                <use href={icons + "#arrowDown"} />
+              </SelectBtnIcon>
+            </SelectBtn>
 
-          <Button style={{ padding: "16px 44px" }}>Search</Button>
-        </Form>
+            <SelectContent ref={dropdownRef}>
+              <SelectOptions isBrandDropDownOpen={isBrandDropDownOpen}>
+                {filters.map((filter) => (
+                  <SelectOptionsItem
+                    key={nanoid()}
+                    onClick={() => {
+                      setBrandValue(filter);
+                    }}
+                    itemValue={filter}
+                    isItemActive={brandValue}
+                  >
+                    {filter}
+                  </SelectOptionsItem>
+                ))}
+              </SelectOptions>
+            </SelectContent>
+          </SelectCarBrandContainer>
+        </FormContainer>
       )}
     </>
   );
 };
 
 export default CatalogFilters;
+
+// <Form>
+//   <Label>
+//     Car brand
+//     <SelectContainer>
+//       <Select>
+//         {filters.map((filter) => (
+//           <Option key={nanoid()}>{filter}</Option>
+//         ))}
+//       </Select>
+//     </SelectContainer>
+//   </Label>
+//   <Label>
+//     Price/1h
+//     <Select>
+//       <Option>To 10$</Option>
+//       <Option>To 20$</Option>
+//       <Option>To 30$</Option>
+//       <Option>To 40$</Option>
+//       <Option>To 50$</Option>
+//     </Select>
+//   </Label>
+//   <div style={{ display: "flex", alignItems: "flex-end" }}>
+//     <Label>
+//       Сar mileage / km
+//       <LeftInput type="text" borderSide={true} placeholder="From" />
+//     </Label>
+//     <RightInput borderSide={false} placeholder="To" />
+//   </div>
+
+//   <Button style={{ padding: "16px 44px" }}>Search</Button>
+// </Form>
